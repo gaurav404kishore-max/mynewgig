f4edb04eaa069f8ee6ce04c5685dbbe304b7bbc2
Gaurav Kishore <gaurav404kishore@gmail.com>
2026-03-13 23:08:22 +0530

    config.js

--- config.js
+++ config.js
@@ -5,5 +5,4 @@
 // ─────────────────────────────────────────────
 
 const SUPABASE_URL = 'https://llzqckypjowuydqsmwhu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsenFja3lwam93dXlkcXNtd2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTQ3OTIsImV4cCI6MjA4ODk5MDc5Mn0.IBggB6d55mDtdL4-p7cfX16Wv2_dxIgEwE2-QH3TXrA
-';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsenFja3lwam93dXlkcXNtd2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTQ3OTIsImV4cCI6MjA4ODk5MDc5Mn0.IBggB6d55mDtdL4-p7cfX16Wv2_dxIgEwE2-QH3TXrA';
